eb599f31366cdd4a9d5b8b14b2e8acd6a0549cb6
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2012-04-17 01:06:07 +0900

    bug fix

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$GIT_REPO=git://github.com/manboubird/dot-files.git 
+GIT_REPO=git://github.com/manboubird/dot-files.git 
 cd &&
 [ -d '.dot-files' ] || git clone "$GIT_REPO" .dot-files &&
 ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
