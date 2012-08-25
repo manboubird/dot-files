321ae8955c06b8454c89b2849900d94fbfb84e1e
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2012-08-25 23:24:28 +0900

    update rsync_dotfiles.sh

--- local/bin/rsync_dotfiles.sh
+++ local/bin/rsync_dotfiles.sh
@@ -17,16 +17,20 @@ done
 
 shift `expr $OPTIND - 1`
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
   echo "One argument of destination {USER}@{HOSTNAME} is required."
   exit 1
 fi
 
-DST=$1
-echo "Rsync dot-files to destination: $DST"
-rsync -auv --delete --exclude ".git" --exclude "rsync_dotfiles_bk"  --backup --backup-dir=rsync_dotfiles_bk/`date +%Y%m%d_%H%M%S` ~/.dot-files/ $DST:~/.dot-files
-if [ "$CREATE_LINK" == "true" ]; then
-  echo "Create symbolic links of dot-files"
-  ssh $DST "bash ~/.dot-files/clone_and_link.sh"
-fi
-echo "Complete rsync to destination: $DST"
+while (($#)); do
+  DST=$1
+  echo "Start rsync dot-files to destination: $DST"
+  rsync -auv --delete --exclude ".git" --exclude ".swq" --exclude "rsync_dotfiles_bk"  --backup --backup-dir=rsync_dotfiles_bk/`date +%Y%m%d_%H%M%S` ~/.dot-files/ $DST:~/.dot-files
+  if [ "$CREATE_LINK" == "true" ]; then
+    echo "Create symbolic links of dot-files"
+    ssh $DST "bash ~/.dot-files/clone_and_link.sh"
+  fi
+  echo "Complete rsync to destination: $DST"
+  echo "---"
+  shift
+done
