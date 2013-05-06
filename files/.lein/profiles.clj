6ff0a45b62ef7e2c92f2a0d71b68fbdbf3d4b722
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2013-05-06 13:16:13 +0900

    update .lein

--- files/.lein/profiles.clj
+++ files/.lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user {:plugins [[org.clojure/tools.nrepl "0.2.2"]
+                  [lein-pprint "1.1.1"]
+                  [lein-kibit "0.0.8"]
+                  [lein-difftest "2.0.0"]
                   [lein-localrepo "0.4.1"]
                   [lein-create-template "0.1.1"]
                   [lein-cascalog "0.1.0"]
