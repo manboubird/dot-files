e4c87046452ec0baf3939518f268a204989cdbbe
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2014-10-13 18:35:01 +0900

    Update clojure setting

--- files/.lein/profiles.clj
+++ files/.lein/profiles.clj
@@ -1,8 +1,11 @@
-{:user {:plugins [[org.clojure/tools.nrepl "0.2.2"]
+{:user {:plugins [
+                  [org.clojure/tools.nrepl "0.2.2"]
                   [lein-pprint "1.1.1"]
                   [lein-kibit "0.0.8"]
                   [lein-difftest "2.0.0"]
-                  [lein-localrepo "0.4.1"]
+                  [lein-localrepo "0.5.3"]
                   [lein-create-template "0.1.1"]
                   [lein-cascalog "0.1.0"]
-                  ]}}
+                  ]
+        ; :repositories [["cdh.releases.repo" "https://repository.cloudera.com/artifactory/cloudera-repos/"]]
+}}
