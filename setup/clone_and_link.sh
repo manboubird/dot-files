1cba55ce9feeffca631535b2a9f1de1f4af058eb
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2018-01-02 12:38:47 +0900

    fix setup script

--- setup/clone_and_link.sh
+++ setup/clone_and_link.sh
@@ -16,15 +16,18 @@ main(){
     echo "Try to clone repository $GIT_REPO ..."
     git clone "$GIT_REPO" "${dot_files}" || exit "$?"
   fi
-  symbolic_link_to_dot_files "$(ls -1d $dot_files/config/* $dot_files/dot/*)"
-  symbolic_link_to_dot_local_bin "$(ls -1d $dot_files/local/bin/*)"
+  symbolic_link_to_dir "$(ls -1d $dot_files/config/*)" "${XDG_CONFIG_HOME}"
+  symbolic_link_to_dot_files "$(ls -1d $dot_files/dot/*)"
+  symbolic_link_to_dir "$(ls -1d $dot_files/local/bin/*)" "${USER_LOCAL_HOME}/bin"
 }
 
 symbolic_link_to_dot_local_bin(){
+symbolic_link_to_dir(){
   local files=$1
-  echo "Create symbolic links to local/bin files ..."
+  local dst_root=${2:-"."}
+  echo "Create symbolic links to directory ..."
   for file in ${files[@]}; do
-    ln -vsf "$file" "${USER_LOCAL_HOME}/bin/"
+    ln -vsf "$file" "${dst_root}/" 
   done
 }
 
