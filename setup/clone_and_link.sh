f61d8457af763eab45dfc461045e242a6eb5f45c
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2018-01-02 12:02:13 +0900

    apply xdg directory structure to setup script

--- setup/clone_and_link.sh
+++ setup/clone_and_link.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
+
 GIT_REPO=${DOT_FILES_GIT_REPO:-"git://github.com/manboubird/dot-files.git"}
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
+XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
+USER_LOCAL_HOME="${USER_LOCAL_HOME:-${HOME}/.local}"
 
-files=".dot-files/files"
-cd &&
-[ -d '.dot-files' ] || git clone "$GIT_REPO" .dot-files &&
-ls -1d $files/* $files/.* | while read f; do
-  [ "$f" == "$files/." ] ||
-  [ "$f" == "$files/.." ] ||
-  [ "$f" == "$files/.git" ] ||
-  [[ "$f" == *".swp" ]] ||
-  ln -vsf "$f" .
-done
+main(){
+  init
+  cd
+  local dot_files="${HOME}/.dot-files"
+  if [ -d "${dot_files}" ]; then
+    echo "The repository already exists. Skip cloning repository."
+  else
+    echo "Try to clone repository $GIT_REPO ..."
+    git clone "$GIT_REPO" "${dot_files}" || exit "$?"
+  fi
+  symbolic_link_to_dot_files "$(ls -1d $dot_files/config $dot_files/dot/*)"
+  symbolic_link_to_dot_local_bin "$(ls -1d $dot_files/local/bin/*)"
+}
 
-mkdir -p "${HOME}/local/bin" 
-bin="${HOME}/.dot-files/local/bin"
-cd && 
-[ -d '.dot-files' ] &&
-ls -1d $bin/* | while read f; do
-  [ "$f" == "$bin/." ] ||
-  [ "$f" == "$bin/.." ] ||
-  [ "$f" == "$bin/.git" ] ||
-  [[ "$f" == *".swp" ]] ||
-  ln -vsf "$f" "${HOME}/local/bin/"
-done
+symbolic_link_to_dot_local_bin(){
+  local files=$1
+  echo "Create symbolic links to local/bin files ..."
+  for file in ${files[@]}; do
+    ln -vsf "$file" "${USER_LOCAL_HOME}/bin/"
+  done
+}
 
+symbolic_link_to_dot_files(){
+  local files=$1
+  echo "Create symbolic links to dot files ..."
+  for file in ${files[@]}; do
+    local filename=$(basename $file)
+    ln -vsf "${file}" ".$filename"
+  done
+}
+
+init(){
+  mkdir -p "${XDG_CONFIG_HOME}" "${XDG_CACHE_HOME}" "${XDG_DATA_HOME}" "${USER_LOCAL_HOME}/bin"
+}
+
+main
+exit 0
